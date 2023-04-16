eb97c04a1d561a4b9799d0f536058893aafd3059
Vincent Vic <vic.coding@gmail.com>
2023-04-16 20:09:45 +0800

    init

---
+++ start/src/main/java/cn/meshed/cloud/template/ProviderApplication.java
@@ -0,0 +1,22 @@
+package cn.meshed.cloud.template;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <h1>启动类</h1>
+ *
+ * @author Vincent Vic
+ * @version 1.0
+ */
+@EnableDubbo(scanBasePackages = {"cn.meshed.cloud.iam.*.remote"})
+@MapperScan("cn.meshed.cloud.template.*.gatewayimpl.database.mapper")
+@SpringBootApplication(scanBasePackages = {"cn.meshed.cloud", "com.alibaba.cola"})
+public class ProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+    }
+}
